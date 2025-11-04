55a353d46ce0e421b0c1de589778e7cb766e8dda
M.D. <138953719+agender-nerd@users.noreply.github.com>
2025-11-04 01:36:40 +0000

    Added Inventory class

---
+++ FantasyMerchantManager/include/fmm/Inventory.h
@@ -0,0 +1,24 @@
+#ifndef FMM_INVENTORY_H
+#define FMM_INVENTORY_H
+
+#include "fmm/Item.h"
+#include <vector>
+#include <memory>
+
+using ItemPointer = std::shared_ptr<Item>;
+
+class Inventory {
+public:
+    Inventory();
+
+    void listAllItems() const;
+    void removeItem(int index);
+
+    void addItem(const ItemPointer& item) { m_items.push_back(item); }
+    const ItemPointer& getItem(int index) const { return m_items.at(index); }
+
+private:
+    std::vector<ItemPointer> m_items;
+};
+
+#endif
---
+++ FantasyMerchantManager/src/Inventory.cpp
@@ -0,0 +1,16 @@
+#include "fmm/Inventory.h"
+#include <iostream>
+
+Inventory::Inventory() {}
+
+void Inventory::listAllItems() const {
+    for(int i = 0; i < m_items.size(); i++) {
+        std::cout << i << ": " << m_items[i]->getName() << std::endl;
+    }
+}
+
+void Inventory::removeItem(int index) {
+    if (index >= 0 && index < m_items.size()) {
+        m_items.erase(m_items.begin() + index);
+    }
+}
